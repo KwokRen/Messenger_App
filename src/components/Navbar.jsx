a4b4440c9ad2058a073c0c72a43afe45899f6a79
JordanC <jcwilliams410@gmail.com>
2020-10-16 16:13:44 -0400

    navbar name using localstorage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 // import Navbar from 'react-bootstrap/Navbar';
 import Icon from '../components/Icon'
+import {getMessages, login} from '../services'
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: 'Jordan'
+            username: '',
+            icon: 'https://www.avisystems.com/hubfs/AVI%20Logos/AVI_logo_CMYK_WhiteRule.svg'
         }
+    }
+
+    componentDidMount = () => {
+        this.populate()
+    }
+
+    populate = async() => {
+        console.log(localStorage.getItem('user'))
+        this.setState({
+            username: localStorage.getItem('user')
+        })
     }
 
     render() {
@@ -15,6 +28,7 @@ class NavBar extends React.Component {
             <div className="navbar">
                 <Icon />
                 <div>{this.state.username}</div>
+                <div><img src={this.state.icon} alt="test"/></div>
                 <div className="logout">Logout</div>
             </div>
         )
